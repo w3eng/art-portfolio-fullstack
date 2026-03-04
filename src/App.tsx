58bd843431c697aca4fb104256b3f12c5f99c23e
Paweł Ziółkowski <pawlok250@wp.pl>
2026-03-04 09:38:43 +0100

    talalaala3343l

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
@@ -8,41 +8,43 @@ import { HeroTitle } from "./components/HeroTitle/herotitle";
 import { ArtSection } from "./components/artsections/artsection1";
 import { ArtSection2 } from "./components/artsections/artsection2";
 
-import { Kontakt } from "./pages/Kontakt.tsx"; // jeśli już masz stronę kontakt
+import { Kontakt } from "./pages/Kontakt.tsx"; 
 import { O_mnie } from "./pages/O_mnie.tsx";
 import { Gallery } from "./pages/Gallery.tsx"; 
 
 function App() {
   return (
-    <div className="min-h-screen bg-white text-gray-900 flex flex-col">
-      
-      <Header />
+    <Router basename="/art-portfolio-fullstack">
+      <div className="min-h-screen bg-white text-gray-900 flex flex-col">
+        
+        <Header />
 
-      <main className="pt-20 flex-grow">
-        <Routes>
-          {/* HOME / LANDING */}
-          <Route
-            path="/art-portfolio-fullstack/"
-            element={
-              <>
-                <Article />
-                <HeroTitle />
-                <ArtSection />
-                <ArtSection2 />
-              </>
-            }
-          />
+        <main className="pt-20 flex-grow">
+          <Routes>
+            {/* HOME / LANDING */}
+            <Route
+              path="/"
+              element={
+                <>
+                  <Article />
+                  <HeroTitle />
+                  <ArtSection />
+                  <ArtSection2 />
+                </>
+              }
+            />
 
-          {/* KONTAKT */}
-          <Route path="/Galeria" element={<Gallery />} />
-          <Route path="/kontakt" element={<Kontakt />} />
-           <Route path="/O_mnie" element={<O_mnie />} />
-        </Routes>
-      </main>
+            {/* KONTAKT */}
+            <Route path="/Galeria" element={<Gallery />} />
+            <Route path="/kontakt" element={<Kontakt />} />
+            <Route path="/O_mnie" element={<O_mnie />} />
+          </Routes>
+        </main>
 
-      <Footer />
+        <Footer />
 
-    </div>
+      </div>
+    </Router>
   );
 }
 
