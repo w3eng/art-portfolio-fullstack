3fd5cef62087694f342f78ace3df7702f1b9c76a
Paweł Ziółkowski <pawlok250@wp.pl>
2026-03-04 10:28:13 +0100

    taa3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
-
 import { Article } from "./components/article/article";
 import { HeroTitle } from "./components/HeroTitle/herotitle";
 import { ArtSection } from "./components/artsections/artsection1";
 import { ArtSection2 } from "./components/artsections/artsection2";
-
 import { Kontakt } from "./pages/Kontakt.tsx"; 
 import { O_mnie } from "./pages/O_mnie.tsx";
 import { Gallery } from "./pages/Gallery.tsx"; 
@@ -15,33 +12,16 @@ import { Gallery } from "./pages/Gallery.tsx";
 function App() {
   return (
     <div className="min-h-screen bg-white text-gray-900 flex flex-col">
-      
       <Header />
-
       <main className="pt-20 flex-grow">
         <Routes>
-          {/* HOME / LANDING */}
-          <Route
-            path="/"
-            element={
-              <>
-                <Article />
-                <HeroTitle />
-                <ArtSection />
-                <ArtSection2 />
-              </>
-            }
-          />
-
-          {/* KONTAKT */}
+          <Route path="/" element={<><Article /><HeroTitle /><ArtSection /><ArtSection2 /></>} />
           <Route path="/Galeria" element={<Gallery />} />
           <Route path="/kontakt" element={<Kontakt />} />
           <Route path="/O_mnie" element={<O_mnie />} />
         </Routes>
       </main>
-
       <Footer />
-
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/art-portfolio-fullstack/', // <- ważne dla GitHub Pages
+  base: '/art-portfolio-fullstack/', // konieczne dla GitHub Pages
 });
