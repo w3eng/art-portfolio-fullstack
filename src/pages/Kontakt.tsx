351c6b9a007404b27f1992f5b5b3963b602290eb
Paweł Ziółkowski <pawlok250@wp.pl>
2026-03-04 08:39:42 +0100

    Aktualizacja fetch do deployu, poprawki kontaktu

--- src/pages/Kontakt.tsx
+++ src/pages/Kontakt.tsx
@@ -25,7 +25,7 @@ export const Kontakt = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/contact", {
+    const response = await fetch("https://art-portfolio-fullstack.onrender.com/api/contact", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, email, message }),
