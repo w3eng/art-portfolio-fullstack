9407a56d276764810baa1493435e3c6478596a3e
Paweł Ziółkowski <pawlok250@wp.pl>
2026-03-04 10:03:34 +0100

    Prepare deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/main/', // <- ważne dla GitHub Pages
+  base: '/art-portfolio-fullstack/', // <- ważne dla GitHub Pages
 });
